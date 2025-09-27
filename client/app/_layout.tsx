19053479ae3cabdcccf74d7b32b42958f1e4a124
qinflan <158785365+qinflan@users.noreply.github.com>
2025-09-27 03:45:40 -0400

    landing screen

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,9 +1,25 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { Text } from "react-native";
 
 export default function RootLayout() {
+
+  const [fontsLoaded] = useFonts({
+  InterRegular: require("../assets/fonts/Inter-Regular.ttf"),
+  InterSemiBold: require("../assets/fonts/Inter-SemiBold.ttf"),
+  });
+  
+  // dont't render app before loading fonts
+  // insert a loading animation pls
+  if (!fontsLoaded) {
+    return (
+      <Text>Loading...</Text>
+    )
+  }
+
   return (
   <Stack>
-    <Stack.Screen name="index" options={{ title: "Curia" }} />
+    <Stack.Screen name="index" options={{ headerShown: false }} />
     <Stack.Screen name="login" options={{ title: "Login" }} />
     <Stack.Screen name="signup" options={{ title: "Sign Up" }} />
   </Stack>
