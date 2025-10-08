dcb6ccf6a451f16e03bb871100bd977994e6fee2
Quinn Flanigan <quinn.flanigan001@umb.edu>
2025-10-07 21:25:27 -0400

    add user types for auth context

--- client/hooks/AuthContext.ts
+++ client/hooks/AuthContext.ts
@@ -1,1 +1,52 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
+import { 
+    registerUser,
+    loginUser,
+    getUser,
+    updateUser,
+    logoutUser,
+    refreshAccessToken,
+} from "@/api/authHandler";
+
+
+export type User = {
+    email: string,
+    id: string,
+    firstName: string,
+    lastName: string,
+    setupComplete: boolean,
+    city?: string,
+    state?: string,
+    dateOfBirth?: Date,
+    preferences?: {
+        interests: [string],
+        subscription: boolean,
+        notifications: boolean,
+        theme: { 
+            type: String, 
+            enum: ['light', 'dark'], 
+            default: 'light' 
+            }
+    }
+}
+
+type AuthContextType = {
+    user: User | null,
+    accessToken: string | null,
+    loading: boolean,
+    login: () => Promise<void>,
+    register: () => Promise<void>,
+    getUser: () => Promise<void>,
+    logout: () => Promise<void>,
+    update: () => Promise<void>,
+    refresh: () => Promise<void>
+}
+
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+
+
+
+
+
